86d346b92e1f77d199f2c670d4cd303fe3c8d12d
GUILHERME ARAUJO DA SILVA <48190207814@info.local>
2026-04-29 08:54:58 -0300

    Merge APi Final

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VHBurguer.Applications.Services;
+using VHBurguer.DTOs.ProdutoDto;
+using VHBurguer.Exceptions;
+
+namespace VHBurguer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoService _service;
+
+        public ProdutoController(ProdutoService service)
+        {
+            _service = service;
+        }
+
+        // autenticação do usuário
+        private int ObterUsuarioIdLogado()
+        {
+            // busca no token/claims o valor armazenado como id do usuário
+            // ClaimTypes.NameIdentifier geralmente guarda o ID do usuário no JWT
+            string? idTexto = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if(string.IsNullOrWhiteSpace(idTexto))
+            {
+                throw new DomainException("Usuário não autenticado");
+            }
+
+            // Converte o ID que veio como texto para inteiro
+            // nosso UsuarioID no sistema está como int
+            // as Claims (informações do usuário dentro do token) sempre são armazenadas como texto.
+            return int.Parse(idTexto);
+        }
+
+        [HttpGet]
+        public ActionResult<List<LerProdutoDto>> Listar()
+        {
+            List<LerProdutoDto> produtos = _service.Listar();
+
+            //return StatusCode(200, produtos);
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<LerProdutoDto> ObterPorId(int id)
+        {
+            LerProdutoDto produto = _service.ObterPorId(id);
+
+            if(produto == null) { 
+                //return StatusCode(404);
+                return NotFound();
+            }
+
+            return Ok(produto);
+        }
+
+        // GET -> api/produto/5/imagem
+        [HttpGet("{id}/imagem")]
+        public ActionResult ObterImagem(int id)
+        {
+            try
+            {
+                var imagem = _service.ObterImagem(id);
+
+                // Retorna o arquivo para o navegador
+                // "image/jpeg" informa o tipo da imagem (MIME type)
+                // O navegador entende que deve renderizar como imagem
+                return File(imagem, "image/jpeg");
+            }
+            catch (DomainException ex)
+            {
+                return NotFound(ex.Message); // NotFound -> não encontrado
+            }
+        }
+
+        [HttpPost]
+        // indica que recebe dados no formato multipart/form-data
+        // necessário quando enviamos arquivos (ex. imagem do produto)
+        [Consumes("multipart/form-data")]
+        [Authorize] // exige login para adicionar produtos
+
+        // [FromForm] -> diz que os dados vem do formulário da requisição (multipart/form-data)
+        public ActionResult Adicionar([FromForm] CriarProdutoDto produtoDto)
+        {
+            try
+            {
+                int usuarioId = ObterUsuarioIdLogado();
+
+                // o cadastro fica associado ao usuário logado
+                _service.Adicionar(produtoDto, usuarioId);
+
+                return StatusCode(201); // Created
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [Consumes("multipart/form-data")]
+        [Authorize]
+        public ActionResult Atualizar(int id, [FromForm] AtualizarProdutoDto produtoDto)
+        {
+            try
+            {
+                _service.Atualizar(id, produtoDto);
+                return NoContent();
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public ActionResult Remover(int id)
+        {
+            try
+            {
+                _service.Remover(id);
+                return NoContent();
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# VH_Burguer
+Aplicação FullStack estruturada com arquitetura web moderna, utilizando .NET/ASP.NET no desenvolvimento de APIs robustas e Next.js com React.js para construção de interfaces dinâmicas e renderização otimizada.
